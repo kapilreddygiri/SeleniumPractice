50261876f3817acafd8570db422985b801589299
kapilreddygiri <45658297+kapilreddygiri@users.noreply.github.com>
2019-02-12 20:15:57 +0530

    My commit

--- SeleniumPractice/src/main/java/com/ibs/practice/App.java
+++ SeleniumPractice/src/main/java/com/ibs/practice/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello IBS OFFSHORE QA TEAM World!" );
+        System.out.println( "Hello IBS QA1 World! Added Kapil" );
     }
 }
