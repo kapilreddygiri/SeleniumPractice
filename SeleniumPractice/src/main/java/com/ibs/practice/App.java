a8add0acb822e860c29be8ae736ae0747e811bc6
kapilreddygiri <45658297+kapilreddygiri@users.noreply.github.com>
2019-02-12 19:22:46 +0530

    My Initial Code Commit

---
+++ SeleniumPractice/src/main/java/com/ibs/practice/App.java
@@ -0,0 +1,13 @@
+package com.ibs.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
