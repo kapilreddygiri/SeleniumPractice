4714a2786eb3e3097a142eec7aed141e06af5bed
Sudharsan <sri2sudharsan91@gmail.com>
2019-02-12 09:29:59 -0500

    sri commit

--- SeleniumPractice/src/main/java/com/ibs/practice/App.java
+++ SeleniumPractice/src/main/java/com/ibs/practice/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello IBS QA World!" );
+        System.out.println( "Hello IBS QA1 World!" );
     }
 }
